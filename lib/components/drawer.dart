e0eca68d4e81f06c2637a6f39c206c3d629a1349
Dias <diasspratamaa24@gmail.com>
2024-09-05 16:46:21 +0700

    [CR] Menampilkan data resep ke dalam drawer

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -15,10 +15,12 @@ class MyDrawer extends StatefulWidget {
 
 class _MyDrawerState extends State<MyDrawer> {
   int recipeCount = 0;
+  List<dynamic> recipes = [];
 
   void initState() {
     super.initState();
     fetchRecipeCount();
+    fetchRecipeData();
   }
 
   Future<void> fetchRecipeCount() async {
@@ -27,6 +29,18 @@ class _MyDrawerState extends State<MyDrawer> {
       final List<dynamic> data = json.decode(response.body);
       setState(() {
         recipeCount = data.length;
+      });
+    } else {
+      throw Exception('Failed to load recipes');
+    }
+  }
+
+  Future<void> fetchRecipeData() async {
+    final response = await http.get(Uri.parse('https://66d17ef762816af9a4f3d884.mockapi.io/recipes'));
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body);
+      setState(() {
+        recipes = data;
       });
     } else {
       throw Exception('Failed to load recipes');
@@ -79,34 +93,29 @@ class _MyDrawerState extends State<MyDrawer> {
               },
             ),
             ListTile(
-              title: const Text(
-                'Recipes',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 65, 179, 162),
-                ),  
-              ),
-              leading: Icon(
-                Icons.book_outlined,
-                color: Color.fromARGB(255, 65, 179, 162),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Recipes(),
+              title: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: const Text(
+                  'Our Recipes',
+                  style: TextStyle(
+                    fontSize: 10,
                   ),
-                );
-              },
+                ),
+              ),
             ),
             Container(
-              height: MediaQuery.of(context).size.height - 300,
+              height: MediaQuery.of(context).size.height - 400,
               child: ListView.builder(
                   itemCount: recipeCount,
                   itemBuilder: (context, index) {
+                    final recipe = recipes[index];
                     return ListTile(
-                      leading: Icon(Icons.bookmark_border_outlined),
-                      title: const Text(
-                        'Nasi Goreng',
+                      leading: Icon(
+                        Icons.bookmark_border_outlined,
+                        color: Color.fromARGB(255, 65, 179, 162),
+                      ),
+                      title: Text(
+                        recipe['name_recipe'] ?? 'No name',
                         style: TextStyle(
                           color: Color.fromARGB(255, 65, 179, 162),
                         ),
@@ -128,106 +137,3 @@ class _MyDrawerState extends State<MyDrawer> {
     );
   }
 }
-
-        // body: ListView(
-        //   padding: EdgeInsets.zero,
-        //   children: [
-        //     const DrawerHeader(
-        //       decoration: BoxDecoration(
-        //         image: DecorationImage(
-        //           image: AssetImage('images/drawerheader_image_copy.jpg'),
-        //           fit: BoxFit.cover,
-        //         ),
-        //       ),
-        //       child: Center(
-        //         child: Text(
-        //           'ResepIN',
-        //           style: TextStyle(
-        //             fontSize: 30,
-        //             fontWeight: FontWeight.bold,
-        //             color: Colors.white,
-        //           ),
-        //         ),
-        //       ),
-        //     ),
-        //     ListTile(
-        //       title: const Text(
-        //         'Home',
-        //         style: TextStyle(
-        //           color: Color.fromARGB(255, 65, 179, 162),
-        //         ),
-        //       ),
-        //       leading: Icon(
-        //         Icons.home_outlined,
-        //         color: Color.fromARGB(255, 65, 179, 162),
-        //       ),
-        //       onTap: () {
-        //         Navigator.push(
-        //           context,
-        //           MaterialPageRoute(builder: (context) => Homepage()),
-        //         );
-        //       },
-        //     ),
-        //     Theme(
-        //       data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
-        //       child: ExpansionTile(
-        //         title: const Text(
-        //           'Recipes',
-        //           style: TextStyle(
-        //             color: Color.fromARGB(255, 65, 179, 162),
-        //           ),
-        //         ),
-        //         leading: Icon(
-        //           Icons.book_outlined,
-        //           color: Color.fromARGB(255, 65, 179, 162),
-        //         ),
-        //         children: [
-        //           Padding(
-        //             padding: const EdgeInsets.only(left: 30),
-        //             child: ListTile(
-        //               title: const Text(
-        //                 'All Recipes',
-        //                 style: TextStyle(
-        //                   color: Color.fromARGB(255, 65, 179, 162),
-        //                 ),
-        //               ),
-        //               leading: Icon(
-        //                 Icons.bookmark_outline,
-        //                 color: Color.fromARGB(255, 65, 179, 162),
-        //               ),
-        //               onTap: () {
-        //                 Navigator.push(
-        //                   context,
-        //                   MaterialPageRoute(builder: (context) => Recipes()),
-        //                 );
-        //               },
-        //             ),
-        //           ),
-        //           Padding(
-        //             padding: const EdgeInsets.only(left: 30),
-        //             child: ListTile(
-        //               title: const Text(
-        //                 'Nasi Goreng',
-        //                 style: TextStyle(
-        //                   color: Color.fromARGB(255, 65, 179, 162),
-        //                 ),
-        //               ),
-        //               leading: Icon(
-        //                 Icons.bookmark_outline,
-        //                 color: Color.fromARGB(255, 65, 179, 162),
-        //               ),
-        //               onTap: () {
-        //                 Navigator.push(
-        //                   context,
-        //                   MaterialPageRoute(
-        //                     builder: (context) => FocusRecipe(),
-        //                   ),
-        //                 );
-        //               },
-        //             ),
-        //           )
-        //         ],
-        //       ),
-        //     )
-        //   ],
-        // ),
